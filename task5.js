3faa52f0b04ac2707fbebf9999b9312590a14e84
sriya518 <114557105+sriya518@users.noreply.github.com>
2023-10-15 16:25:54 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,67 @@
+console.log("hello everyone");
+
+let var1 = 5;
+
+let var2 = 8;
+
+console.log(var2);
+
+let age_of_a_person = 21;
+
+let ageOfAPerson = 20;
+
+console.log(ageOfAPerson);
+
+const pannumber = 12345;
+
+console.log(pannumber);
+
+let number1 = 145;
+let number2 = 147;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+console.log(number1 ** number2);
+
+
+let age = 26;
+age++;
+
+let android = 82;
+android--;
+
+console.log(age);
+console.log(android);
+
+let rays = 98;
+
+rays += 9;
+console.log(rays);
+rays -= 8;
+console.log(rays);
+rays *= 11;
+console.log(rays);
+rays /= 5;
+console.log(rays);
+rays %= 7;
+console.log(rays);
+
+let number11 = 75;
+let number22 = 57;
+
+console.log(number11==number22);
+console.log(number11!=number22);
+console.log(number11===number22);
+console.log(number11<number22);
+console.log(number11>number22);
+console.log(number11<=number22);
+console.log(number11>=number22);
+
+let number6 = 75;
+let number7 = "75";
+
+console.log(number6==number7);
+console.log(number6===number7);
